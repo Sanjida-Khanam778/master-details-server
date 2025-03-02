630fdb1a68b7cced3d89c6cfa5d6ac94c13a4a91
Sanjida <sanjida.ice.778@gmail.com>
2025-03-03 02:42:14 +0600

    update a department

--- index.js
+++ index.js
@@ -22,22 +22,48 @@ app.get("/students/:departmentId", async (req, res) => {
 });
 
 app.post("/departments", async (req, res) => {
-  const {department_name, department_code, head_of_department, description, created_at} = req.body;
-  const result = await pool.query("INSERT INTO department(department_name, department_code, head_of_department, description, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING *", [department_name, department_code, head_of_department, description, created_at])
-  res.status(201).json(result.rows[0])
+  const {
+    department_name,
+    department_code,
+    head_of_department,
+    description,
+    created_at,
+  } = req.body;
+  const result = await pool.query(
+    "INSERT INTO department(department_name, department_code, head_of_department, description, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    [
+      department_name,
+      department_code,
+      head_of_department,
+      description,
+      created_at,
+    ]
+  );
+  res.status(201).json(result.rows[0]);
 });
 
-app.delete('/departments/:id', async(req,res)=>{
-    const {id} = req.params
-    const result = await pool.query("DELETE FROM department WHERE id = $1 RETURNING *", [id])
-    res.json(result.rows[0])
-})
+app.delete("/departments/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await pool.query(
+    "DELETE FROM department WHERE id = $1 RETURNING *",
+    [id]
+  );
+  res.json(result.rows[0]);
+});
 
-app.put('/departments/:id', async(req,res)=>{
-    const {id} = req.params
-    const result = await pool.query("UPDATE department SET department_name=$1, department_code=$2, head_of_department=$3, description=$4 WHERE id = $5 RETURNING *", [department_name, department_code, head_of_department, description, id])
-    res.json(result.rows)
-})
+app.put("/departments/:id", async (req, res) => {
+  const { id } = req.params;
+  const {department_name, department_code, head_of_department, description} = req.body
+  const result = await pool.query(
+    "UPDATE department SET department_name=$1, department_code=$2, head_of_department=$3, description=$4 WHERE id = $5 RETURNING *",
+    [department_name, department_code, head_of_department, description, id]
+  );
+  res.json(result.rows[0]);
+});
+
+// students
+
+
 
 app.get("/", async (req, res) => {
   res.send("Server is running...");
