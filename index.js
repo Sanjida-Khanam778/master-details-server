d4ed7b7a12df33652b166a3e878ffa1f6743b736
Sanjida <sanjida.ice.778@gmail.com>
2025-03-02 23:25:53 +0600

    add a post route

--- index.js
+++ index.js
@@ -6,19 +6,31 @@ app.use(express.json());
 app.use(cors());
 
 // Get all departments
-app.get('/departments', async (req, res) => {
-    const result = await pool.query('SELECT * FROM department');
-    res.json(result.rows);
+app.get("/departments", async (req, res) => {
+  const result = await pool.query("SELECT * FROM department");
+  res.json(result.rows);
 });
 
 // Get students by department ID
-app.get('/students/:departmentId', async (req, res) => {
-    const departmentId = parseInt(req.params.departmentId, 10);
-    const result = await pool.query('SELECT * FROM student WHERE department_id = $1', [departmentId]);
-    res.json(result.rows);
+app.get("/students/:departmentId", async (req, res) => {
+  const departmentId = parseInt(req.params.departmentId, 10);
+  const result = await pool.query(
+    "SELECT * FROM student WHERE department_id = $1",
+    [departmentId]
+  );
+  res.json(result.rows);
 });
 
+app.post("/departments", async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.send(data);
+});
+
+app.get("/", async (req, res) => {
+  res.send("Server is running...");
+});
 
 app.listen(5000, async (req, res) => {
-    console.log("running on port: 5000");
-  });
+  console.log("running on port: 5000");
+});
